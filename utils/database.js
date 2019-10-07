9852f4abaca85e0881d57f1b3360b69162347c1d
Okoro Ikechukwu <ikeshegs@gmail.com>
2019-10-07 12:05:56 +0100

    ft: setup heroku database

--- utils/database.js
+++ utils/database.js
@@ -5,10 +5,27 @@ dotenv.config();
 
 let sequelize;
 
+// if (process.env.NODE_ENV = 'development') {
+//   sequelize = new Sequelize(process.env.DB_URI);
+// } else {
+//   sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_PROD2_URI);
+// }
+
 if (process.env.NODE_ENV = 'development') {
-  sequelize = new Sequelize(process.env.DB_URI);
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_PROD2_URI);
+  sequelize = new Sequelize(process.env.DB_PROD_NAME, process.env.DB_PROD_USERNAME, process.env.DB_PROD_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
 }
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 
 module.exports = sequelize;
