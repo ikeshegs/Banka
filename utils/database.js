0bcd1df37052d4b8faac6dd6e4dcd73661fbe1fe
Okoro Ikechukwu <ikeshegs@gmail.com>
2019-10-07 13:09:35 +0100

    bg: use elephantsql for production database connection

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,10 @@ const Sequelize = require('sequelize');
 
 dotenv.config();
 
-let sequelize = new Sequelize(process.env.DB_URI) || new Sequelize(process.env.DB_PROD_URI);
+let sequelize = new Sequelize(process.env.DB_URI) || new Sequelize(process.env.DB_DEV_NAME, process.env.DB_DEV_USERNAME, process.env.DB_DEV_PASSWORD, {
+  host: process.env.DB_DEV_HOST,
+  dialect: 'postgres'
+});
 
 // if (process.env.NODE_ENV = 'development') {
 //   sequelize = new Sequelize(process.env.DB_PROD_URI);
